a62aed6fc8ff081480bc3ca3c0c668dfd94c9f7e
Camilo-Fetiva <kmilofetiva@gmail.com>
2024-10-23 21:28:52 -0500

    [actualizar] mongoose

--- app.js
+++ app.js
@@ -4,10 +4,17 @@
 
 // 1. Importar las dependencias y módulos necesarios 
 import express from 'express'; //ECMAS 6
+import dotenv from 'dotenv'; //Dependencia para manejar variables de entorno
+import { connectionMongo } from './src/config/dataBase.js';
+
 
 // 2. CONFIGURAR EL USO DEL SERVIDOR
 const app = express();
-const port = 6000; //3000, 6000 o 9000
+dotenv.config(); //configurar para poder usar variables de entorno
+const port = process.env.PORT//3000, 6000 o 9000
+
+//INVOCAR LA FUNCION DE LA BASE DE DATOS
+connectionMongo ();
 
 // 3. EJECUTAR EL SERVIDOR EN EL COMPUTADOR
 app.listen(port, () => {
